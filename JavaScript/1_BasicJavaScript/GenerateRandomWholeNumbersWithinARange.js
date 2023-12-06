bf7a7a8ec675ae8545e14421b1b6915f653062f5
AndreyUrazaev <f.a.z.a@bk.ru>
2023-12-06 16:34:01 +0500

    BasicJavaScript (113/113)

---
+++ JavaScript/1_BasicJavaScript/GenerateRandomWholeNumbersWithinARange.js
@@ -0,0 +1,8 @@
+// Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin and less than or equal to myMax.
+
+function randomRange(myMin, myMax) {
+	return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+	// return Math.round(Math.random() * (myMax - myMin)) + myMin;
+}
+
+console.log(randomRange(2, 5))
